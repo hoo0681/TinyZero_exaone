e260f2f55ca6fe68e0669782156625b10bd0f88e
seunghoo hong <hoo0681@naver.com>
2025-01-31 20:30:21 +0900

    .

--- scripts/2A100.sh
+++ scripts/2A100.sh
@@ -1,7 +1,7 @@
 export N_GPUS=2
 export CUDA_VISIBLE_DEVICES=0,1
 # ray stop --force && ray start --head --include-dashboard=True
-export BASE_MODEL="model/Qwen2.5-3B"
+export BASE_MODEL=Qwen/Qwen2.5-3B
 export DATA_DIR="dataset"
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-3b-grpo
